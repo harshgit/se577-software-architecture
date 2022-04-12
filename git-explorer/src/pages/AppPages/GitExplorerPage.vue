8e373df243f574b4c6aee29b3e7d33feb03a9cd8
hbolak650 <harsh_bolakani@cable.comcast.com>
2022-04-12 16:10:58 -0400

    initial commit

---
+++ git-explorer/src/pages/AppPages/GitExplorerPage.vue
@@ -0,0 +1,41 @@
+
+
+
+<script lang="ts">
+import ExampleComponent from 'components/ExampleComponent.vue';
+import { Meta, Todo } from 'components/models';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'GitExplorerPage',
+  components: { ExampleComponent },
+  data() {
+    const todos: Todo[] = [
+      {
+        id: 1,
+        content: 'ct1'
+      },
+      {
+        id: 2,
+        content: 'ct2'
+      },
+      {
+        id: 3,
+        content: 'ct3'
+      },
+      {
+        id: 4,
+        content: 'ct4'
+      },
+      {
+        id: 5,
+        content: 'ct5'
+      }
+    ];
+    const meta: Meta = {
+      totalCount: 1200
+    };
+    return { todos, meta };
+  }
+});
+</script>
